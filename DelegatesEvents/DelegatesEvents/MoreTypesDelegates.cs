a9dbd5ec2af01d644b488703e3479ae6869d84d9
marques-henrique <37425086+MarquesRick@users.noreply.github.com>
2024-08-10 13:24:14 -0300

    feat: delegates and events

---
+++ DelegatesEvents/DelegatesEvents/MoreTypesDelegates.cs
@@ -0,0 +1,56 @@
+﻿namespace DelegatesEvents
+{
+    public delegate void Notification();
+    public class MoreTypesDelegates
+    {
+        private List<MoreTypesDelegates> _items = new();
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public DateTime CreatedDate => DateTime.Now;
+        public IReadOnlyCollection<MoreTypesDelegates> GetMoreTypesDelegates => _items;
+        public event Notification Notify;
+
+        #region Event Delegate
+        public void Add(string item)
+        {
+            var itemList = new MoreTypesDelegates();
+            itemList.Id += this._items.Count;
+            itemList.Item = item;
+            this._items.Add(itemList);
+            EventHandler();
+        }
+
+        private void EventHandler()
+        {
+            Notify();
+        }
+        #endregion
+        public override string ToString()
+        {
+            return $"{this.Id} - {this.Item} - {this.CreatedDate}";
+        }
+
+        #region Func
+        public MoreTypesDelegates GetItemById(Func<MoreTypesDelegates, bool> func)
+        {
+            return this._items.FirstOrDefault(func);
+        }
+        #endregion
+        #region Action
+        public void PrintAllItems(Action<MoreTypesDelegates> action)
+        {
+            this._items.ForEach(action);
+        }
+        #endregion
+
+        #region Predicate
+        public bool ExistItem(Predicate<MoreTypesDelegates> pred)
+        {
+            return this._items.Exists(pred);
+        }
+        #endregion
+
+
+
+    }
+}
---
+++ DelegatesEvents/DelegatesEvents/Program.cs
@@ -0,0 +1,65 @@
+﻿using DelegatesEvents;
+
+#region Events & Delegates
+Console.WriteLine("######### Delegates and Events ######### ");
+Console.WriteLine();
+var figura = new GeometricFigure()
+{
+    Height = 10,
+    Width = 10,
+    Depth = 10
+};
+
+figura.Calculate += CalculateSquareArea;
+figura.Calculate += CalculateVolumeCube;
+figura.EventHandler();
+
+static void CalculateSquareArea(double height, double width, double depth)
+{
+    var area = height * width;
+    Console.WriteLine("Event triggered from the client class, square area calculation");
+    Console.WriteLine(area);
+}
+
+static void CalculateVolumeCube(double height, double width, double depth)
+{
+    var volume = height * width * depth;
+    Console.WriteLine("Event triggered from the client class, volume of cube calculation");
+    Console.WriteLine(volume);
+}
+#endregion
+
+#region Func, Action & Predicate
+Console.WriteLine("######### Func, Action & Predicate ######### ");
+Console.WriteLine();
+var moreTypesList = new MoreTypesDelegates();
+
+//multicast delegate -> more than one delegates
+moreTypesList.Notify += Notify;
+moreTypesList.Notify += NotifyAgain;
+moreTypesList.Add("A");
+moreTypesList.Add("B");
+moreTypesList.Add("C");
+
+Action<MoreTypesDelegates> action = new Action<MoreTypesDelegates>(i => Console.WriteLine("Action: " + i));
+Func<MoreTypesDelegates, bool> func = new Func<MoreTypesDelegates, bool>(i => i.Id == 1);
+Predicate<MoreTypesDelegates> pred = new Predicate<MoreTypesDelegates>(i => i.Id == 3);
+Console.WriteLine();
+moreTypesList.PrintAllItems(action);
+Console.WriteLine();
+var f = moreTypesList.GetItemById(func);
+Console.WriteLine("Func: " + f);
+Console.WriteLine();
+var exist = moreTypesList.ExistItem(pred);
+Console.WriteLine("Predicate: " + exist);
+Console.WriteLine();
+static void Notify()
+{
+    Console.WriteLine($"I've been notified");
+}
+
+static void NotifyAgain()
+{
+    Console.WriteLine($"I was notified again");
+}
+#endregion
